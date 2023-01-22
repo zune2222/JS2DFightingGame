d1b3af46880e72501a4b7873e7bd974ad1bde978
zune2222 <zun_e@kakao.com>
2023-01-22 13:31:16 +0000

    Create Player and Enemy

--- main.js
+++ main.js
@@ -4,26 +4,65 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-
+const gravity=0.2
 c.fillRect(0,0,canvas.width,canvas.height);
 
 
 class Sprite {
-	constructor(position){
+	constructor({position,velocity}){
 		this.position=position
+		this.velocity=velocity
+		this.height=150
 	}
 	draw(){
 		c.fillStyle='red'
-		c.fillRect(this.position.x, this.position.y,50,150)
+		c.fillRect(this.position.x, this.position.y,50,this.height)
+	}
+	update(){
+		this.draw()
+		this.position.y += this.velocity.y
+		
+		if(this.position.y + this.height + this.velocity.y >= canvas.height){
+			this.velocity.y=0
+		}
+		else{
+			this.velocity.y+=gravity
+		}
 	}
 }
 
 const player= new Sprite({
-	x:0,
-	y:0
+	position:{
+		x:0,
+		y:0
+	},
+	velocity:{
+		x:0,
+		y:0
+	}
 })
 
-player.draw();
+const enemy = new Sprite({
+	position:{
+		x:400,
+		y:100
+	},
+	velocity:{
+		x:0,
+		y:0
+	}
+})
 
+
+
+function animate(){
+	window.requestAnimationFrame(animate)
+	c.fillStyle='black'
+	c.clearRect(0,0,canvas.width,canvas.height)
+	player.update()
+	enemy.update()
+}
+
+animate()
 
  
